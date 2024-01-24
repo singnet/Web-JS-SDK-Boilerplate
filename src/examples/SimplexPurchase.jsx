bc05dc723afe6770bbf086df766183f3e9db038a
SHAFIN SHAIKH <shafinshaikh25@gmail.com>
2024-01-24 17:36:53 +0530

    simplex production url

--- src/examples/SimplexPurchase.jsx
+++ src/examples/SimplexPurchase.jsx
@@ -2,30 +2,53 @@ import React, { useEffect } from 'react';
 
 const SimplexPurchase = () => {
   useEffect(() => {
-    const simplexScriptId = 'simplex-script';
+    // ID for the simplex form script
+    const simplexFormScriptId = 'simplex-form-script';
+    // ID for the simplex checkout script
+    const simplexCheckoutScriptId = 'simplex-checkout-script';
 
-    // Check if the script already exists, if so, do not add it again
-    if (!document.getElementById(simplexScriptId)) {
-      // Create the script only if it does not exist
-      const script = document.createElement('script');
-      script.id = simplexScriptId; // Assign a unique ID to the script
-      script.src = 'https://iframe.sandbox.test-simplexcc.com/form.js';
-      script.async = true;
-      document.body.appendChild(script);
+    // Load Simplex Form Script
+    const loadSimplexFormScript = () => {
+      if (!document.getElementById(simplexFormScriptId)) {
+        const script = document.createElement('script');
+        script.id = simplexFormScriptId;
+        script.src = 'https://iframe.simplex-affiliates.com/form.js';
+        script.async = true;
+        document.body.appendChild(script);
 
-      // Initialize the Simplex form after the script is loaded
-      script.onload = () => {
-        if (window.simplex) {
-          window.simplex.createForm();
-        }
-      };
-    }
+        script.onload = () => {
+          if (window.simplex) {
+            window.simplex.createForm();
+          }
+        };
+      }
+    };
+
+    // Load Simplex Checkout Script
+    const loadSimplexCheckoutScript = () => {
+      if (!document.getElementById(simplexCheckoutScriptId)) {
+        const script = document.createElement('script');
+        script.id = simplexCheckoutScriptId;
+        script.src = 'https://checkout.simplexcc.com/splx.js';
+        script.async = true;
+        document.body.appendChild(script);
+      }
+    };
+
+    // Load both scripts
+    loadSimplexFormScript();
+    loadSimplexCheckoutScript();
 
     return () => {
-      // Remove the script when the component unmounts
-      const scriptToRemove = document.getElementById(simplexScriptId);
-      if (scriptToRemove) {
-        scriptToRemove.remove();
+      // Remove the scripts when the component unmounts
+      const formScriptToRemove = document.getElementById(simplexFormScriptId);
+      if (formScriptToRemove) {
+        formScriptToRemove.remove();
+      }
+
+      const checkoutScriptToRemove = document.getElementById(simplexCheckoutScriptId);
+      if (checkoutScriptToRemove) {
+        checkoutScriptToRemove.remove();
       }
     };
   }, []);
