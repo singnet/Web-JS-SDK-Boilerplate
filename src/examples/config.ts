f3b5e8904b231659cb5a3385dd282b961a426a8f
Rahul Yadav <rahulkyadav57@gmail.com>
2024-04-27 15:21:57 +0530

    Update config.ts with new RPC endpoint

--- src/examples/config.ts
+++ src/examples/config.ts
@@ -4,4 +4,5 @@ export const config = {
     "ipfsEndpoint": "https://ipfs.singularitynet.io",
     "defaultGasPrice": "8700000",
     "defaultGasLimit": "410000",
+    "rpcEndpoint": "https://mainnet.infura.io/v3/1c7f5b09f1b84b6b8f7f0f1b3f7b3b1d",
 }
